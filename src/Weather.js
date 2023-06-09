bbb9002fde26dede0a302a0612759758f86f0bf9
Uzoma2 <uzomaudeagwu@gmail.com>
2023-06-09 19:23:19 +0100

    Added API integration

--- src/Weather.js
+++ src/Weather.js
@@ -1,83 +1,111 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 export default function Weather() {
-  return (
-    <div className="wrap">
-      <header className="header">
-        <section className="city-suggestions">
-          <a href="/">Jos</a>
-          <a href="/">Quebec</a>
-          <a href="/">Cairo</a>
-          <a href="/">Accra</a>
-          <a href="/">Lisbon</a>
-        </section>
-        <div className="search-current-wrap">
-          <form className="search-form">
-            <input
-              type="search"
-              className="city-input"
-              placeholder="City"
-              autoComplete="off"
-            />
+  const [weather, setWeather] = useState({ loaded: false });
 
-            <input type="submit" className="btn-warning" value="Search" />
-          </form>
-          <div className="current-btn">
-            <input type="submit" className="btn-warning" value="Current" />
-          </div>
-        </div>
-      </header>
-      <main className="main">
-        <div className="temp-today-row">
-          <div className="temp-today">
-            <h1 className="current-city">Abuja</h1>
-            <h3 className="current-day-time">4:30am</h3>
-            <h3 className="weather-description">Partially cloudy</h3>
-            <div className="weather-icon-link-wrap">
-              <img
-                src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-                alt=""
-                className="weather-icon"
+  function showWeather(response) {
+    setWeather({
+      loaded: true,
+      city: response.data.name,
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      description: response.data.weather[0].description,
+    });
+  }
+
+  if (weather.loaded) {
+    return (
+      <div className="wrap">
+        <header className="header">
+          <section className="city-suggestions">
+            <a href="/">Jos</a>
+            <a href="/">Quebec</a>
+            <a href="/">Cairo</a>
+            <a href="/">Accra</a>
+            <a href="/">Lisbon</a>
+          </section>
+          <div className="search-current-wrap">
+            <form className="search-form">
+              <input
+                type="search"
+                className="city-input"
+                placeholder="City"
+                autoComplete="off"
               />
-              <h2>
-                <span className="current-temp-number">30</span>
-                <span className="units">°C </span>
-              </h2>
+
+              <input type="submit" className="btn-warning" value="Search" />
+            </form>
+            <div className="current-btn">
+              <input type="submit" className="btn-warning" value="Current" />
             </div>
           </div>
-          <div className="weathertypes">
-            <h3>
-              Humidity: <span className="humidity-number">41</span>%
-            </h3>
-            <h3>
-              Wind: <span className="wind-number">11</span>km/h
-            </h3>
+        </header>
+        <main className="main">
+          <div className="temp-today-row">
+            <div className="temp-today">
+              <h1 className="current-city">{weather.city}</h1>
+              <h3 className="current-day-time">4:30am</h3>
+              <h3 className="weather-description">{weather.description}</h3>
+              <div className="weather-icon-link-wrap">
+                <img
+                  src={weather.icon}
+                  alt={weather.description}
+                  className="weather-icon"
+                />
+                <h2>
+                  <span className="current-temp-number">
+                    {Math.round(weather.temperature)}
+                  </span>
+                  <span className="units">°C </span>
+                </h2>
+              </div>
+            </div>
+            <div className="weathertypes">
+              <h3>
+                Humidity:{" "}
+                <span className="humidity-number">{weather.humidity}</span>%
+              </h3>
+              <h3>
+                Wind:{" "}
+                <span className="wind-number">{Math.round(weather.wind)}</span>
+                km/h
+              </h3>
+            </div>
           </div>
-        </div>
 
-        <div className="forecast"></div>
-      </main>
-      <footer className="footer">
-        <p>
-          Page was built by
+          <div className="forecast"></div>
+        </main>
+        <footer className="footer">
+          <p>
+            Page was built by
+            <a
+              href="https://www.linkedin.com/in/uzoma-udeagwu"
+              className="footerlink"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Uzoma Udeagwu
+            </a>
+          </p>
           <a
-            href="https://www.linkedin.com/in/uzoma-udeagwu"
+            href="https://github.com/Uzoma2/weather-app-react"
             className="footerlink"
             target="_blank"
             rel="noreferrer"
           >
-            Uzoma Udeagwu
+            Open Source Code
           </a>
-        </p>
-        <a
-          href="https://github.com/Uzoma2/weather-app-react"
-          className="footerlink"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open Source Code
-        </a>
-      </footer>
-    </div>
-  );
+        </footer>
+      </div>
+    );
+  } else {
+    let city = "paris";
+    let apiKey = "094780c710fa4efd669f0df8c3991927";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(showWeather);
+    return `loading...`;
+  }
 }
